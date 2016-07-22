d3630a5f627c8decd7745b596af59bd979771e53
Jeffery-Cai <1345199080@qq.com>
2016-07-22 11:20:14 +0800

    新增获取当前所在位置（省份，城市，国家）

--- BasicsAction.class.php
+++ BasicsAction.class.php
@@ -446,5 +446,78 @@ class BasicsAction extends UserAction
 			}
 			return $no;
 		}
+		
+		/** 
+		* 取汉字的第一个字的首字母 
+		* @param type $str 
+		* @return string|null 
+		*/  
+	   public function _getFirstCharter($str){  
+			if(empty($str)){return '';}  
+			$fchar=ord($str{0});  
+			if($fchar>=ord('A')&&$fchar<=ord('z')) return strtoupper($str{0});  
+			$s1=iconv('UTF-8','gb2312',$str);  
+			$s2=iconv('gb2312','UTF-8',$s1);  
+			$s=$s2==$str?$s1:$str;  
+			$asc=ord($s{0})*256+ord($s{1})-65536;  
+			if($asc>=-20319&&$asc<=-20284) return 'A';  
+			if($asc>=-20283&&$asc<=-19776) return 'B';  
+			if($asc>=-19775&&$asc<=-19219) return 'C';  
+			if($asc>=-19218&&$asc<=-18711) return 'D';  
+			if($asc>=-18710&&$asc<=-18527) return 'E';  
+			if($asc>=-18526&&$asc<=-18240) return 'F';  
+			if($asc>=-18239&&$asc<=-17923) return 'G';  
+			if($asc>=-17922&&$asc<=-17418) return 'H';  
+			if($asc>=-17417&&$asc<=-16475) return 'J';  
+			if($asc>=-16474&&$asc<=-16213) return 'K';  
+			if($asc>=-16212&&$asc<=-15641) return 'L';  
+			if($asc>=-15640&&$asc<=-15166) return 'M';  
+			if($asc>=-15165&&$asc<=-14923) return 'N';  
+			if($asc>=-14922&&$asc<=-14915) return 'O';  
+			if($asc>=-14914&&$asc<=-14631) return 'P';  
+			if($asc>=-14630&&$asc<=-14150) return 'Q';  
+			if($asc>=-14149&&$asc<=-14091) return 'R';  
+			if($asc>=-14090&&$asc<=-13319) return 'S';  
+			if($asc>=-13318&&$asc<=-12839) return 'T';  
+			if($asc>=-12838&&$asc<=-12557) return 'W';  
+			if($asc>=-12556&&$asc<=-11848) return 'X';  
+			if($asc>=-11847&&$asc<=-11056) return 'Y';  
+			if($asc>=-11055&&$asc<=-10247) return 'Z';  
+			return null;  
+			
+			
+			/* 新增取首字母排序 */
+			$settlesRes = array();  
+			foreach ($list as $v) {  
+				$data = $this->_getFirstCharter($v['name']); //取出门店的第一个汉字的首字母  
+				$settlesRes[$data] = $v;//以这个首字母作为key  
+			}
+			ksort($settlesRes); //对数据进行ksort排序，以key的值升序排序
+	   }
+	   
+	/* 获取当前所在位置 ( 省，城市，国家 ) */
+	public function getAddressFromIp(){
+	    $urlTaobao = 'http://ip.taobao.com/service/getIpInfo.php';
+	    $urlSina = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json';
+	    $json = file_get_contents($urlTaobao);
+	    $jsonDecode = json_decode($json);
+	    if($jsonDecode->code==0){//如果取不到就去取新浪的
+	        $data['country'] = $jsonDecode->data->country;
+	        $data['province'] = $jsonDecode->data->region;
+	        $data['city'] = $jsonDecode->data->city;
+	        $data['isp'] = $jsonDecode->data->isp;
+	        return $data;
+	    }else{
+	        $json = file_get_contents($urlSina);
+	        $jsonDecode = json_decode($json);
+	        $data['country'] = $jsonDecode->country;
+	        $data['province'] = $jsonDecode->province;
+	        $data['city'] = $jsonDecode->city;
+	        $data['isp'] = $jsonDecode->isp;
+	        $data['district'] = $jsonDecode->district;
+	        return $data;
+	    }
+	}
+		
 
 }
