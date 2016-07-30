0c3c325bde3b3f8c6538ada5ea2c0487ce660c48
Jeffery-Cai <1345199080@qq.com>
2016-07-30 16:47:05 +0800

    新增php加密/解密字符串

--- BasicsAction.class.php
+++ BasicsAction.class.php
@@ -527,5 +527,39 @@ class BasicsAction extends UserAction
 		return M('table')->order('convert(firm_name using gb2312) asc')->select();
 	}
 	
+	
+	/* 加密 - 解密 */
+		/**
+		 * 简单对称加密算法之加密
+		 * @param String $string 需要加密的字串
+		 * @param String $skey 加密EKY
+		 * @author Anyon Zou <zoujingli@qq.com>
+		 * @date 2013-08-13 19:30
+		 * @update 2014-10-10 10:10
+		 * @return String
+		 */
+		public function encode($string = '', $skey = 'cxphp') {
+		    $strArr = str_split(base64_encode($string));
+		    $strCount = count($strArr);
+		    foreach (str_split($skey) as $key => $value)
+		        $key < $strCount && $strArr[$key].=$value;
+		    return str_replace(array('=', '+', '/'), array('O0O0O', 'o000o', 'oo00o'), join('', $strArr));
+		}
+		/**
+		 * 简单对称加密算法之解密
+		 * @param String $string 需要解密的字串
+		 * @param String $skey 解密KEY
+		 * @author Anyon Zou <zoujingli@qq.com>
+		 * @date 2013-08-13 19:30
+		 * @update 2014-10-10 10:10
+		 * @return String
+		 */
+		public function decode($string = '', $skey = 'cxphp') {
+		    $strArr = str_split(str_replace(array('O0O0O', 'o000o', 'oo00o'), array('=', '+', '/'), $string), 2);
+		    $strCount = count($strArr);
+		    foreach (str_split($skey) as $key => $value)
+		        $key <= $strCount  && isset($strArr[$key]) && $strArr[$key][1] === $value && $strArr[$key] = $strArr[$key][0];
+		    return base64_decode(join('', $strArr));
+		}
 
 }
